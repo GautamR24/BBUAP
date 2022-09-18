c2ca038d1137cec1eac4f13937364741f57bbfa1
GAUTAM RAWAT <rawatgautam24@protonmail.com>
2022-09-18 18:53:50 +0530

    Delete Project directory

--- Project/user_info.py
+++
@@ -1,7 +0,0 @@
-
-
-def get_user_info():
-	Name = input("enter your name")
-	Device_id = input("enter your unique device id")
-	user_info = [Name,Device_id]
-	return user_info
--- Project/user_input.py
+++
@@ -1,5 +0,0 @@
-
-
-def get_user_input():
-	user_input = input("enter your choice")
-	return user_input
